0f69a11a44b33e8fb378441c563c28f7fa37ec3e
jmalek <jan.malek@gmail.com>
2024-04-07 20:51:07 +0100

    chore: More of the same

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -14,18 +14,14 @@ exactly() { # exactly N name search [mode] [filter]
 	num="$(grep "$mode" "$search" $filter | wc -l || true)"
 
 	if [ $num -eq $count ]; then
-		echo "[1;32mOK[0m $num $name"
+		echo "$num $name"
 	else
-		echo "[1;31mFAIL[0m $num $name (expecting exactly $count)"
-		if [ $1 -lt 30 ]; then
-			echo "entries:"
-			grep "$mode" "$search" $filter
-		fi
+		echo "$(tput setaf 9)$num $name (expecting exactly $count)$(tput sgr0)"
 		FAILED=1
 	fi
 }
 
-# If you increase any of these numbers you're probably doing it wrong
+# With the potential exception of << if you increase any of these numbers you're probably doing it wrong
 exactly 0 "escapes" '\\\\(red|blue|green|black|b|i[^mc])'
 exactly 4 "Del()s" '\WDel\('
 exactly 2 "/atom text paths" '"/atom'
@@ -34,21 +30,21 @@ exactly 2 "/datum text paths" '"/datum'
 exactly 2 "/mob text paths" '"/mob'
 exactly 10 "/obj text paths" '"/obj'
 exactly 8 "/turf text paths" '"/turf'
-exactly 141 "to_world uses" '\sto_world\('
+exactly 153 "to_world uses" '\sto_world\('
 exactly 0 "globals with leading /" '^/var' -P
 exactly 0 "globals without global sugar" '^var/(?!global/)' -P
 exactly 0 "apparent paths with trailing /" '\w/[,\)\n]' -P
-exactly 55 "to_world_log uses" '\sto_world_log\('
-exactly 0 "world<< uses" 'world<<|world[[:space:]]<<'
-exactly 0 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
-exactly 2 "<< uses" '(?<!<)<<(?!<)' -P
-exactly 2 ">> uses" '(?<!>)>>(?!>)' -P
+exactly 82 "to_world_log uses" '\sto_world_log\('
+exactly 1 "world<< uses" 'world<<|world[[:space:]]<<'
+exactly 1 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
+exactly 14 "<< uses" '(?<!<)<<(?!<)' -P
+exactly 3 ">> uses" '(?<!>)>>(?!>)' -P
 exactly 0 "incorrect indentations" '^( {4,})' -P
-exactly 25 "text2path uses" 'text2path'
-exactly 5 "update_icon() override" '/update_icon\((.*)\)'  -P
-exactly 4 "goto use" 'goto '
+exactly 37 "text2path uses" 'text2path'
+exactly 3 "update_icon() override" '/update_icon\((.*)\)'  -P
+exactly 5 "goto use" 'goto '
 exactly 1 "NOOP match" 'NOOP'
-exactly 425 "spawn uses" '^\s*spawn\s*\(\s*(-\s*)?\d*\s*\)' -P
+exactly 461 "spawn uses" '^\s*spawn\s*\(\s*(-\s*)?\d*\s*\)' -P
 exactly 0 "tag uses" '\stag = ' -P '**/*.dmm'
 exactly 0 "anchored = 0/1" 'anchored\s*=\s*\d' -P
 exactly 2 "density = 0/1" 'density\s*=\s*\d' -P
@@ -56,7 +52,7 @@ exactly 0 "emagged = 0/1" 'emagged\s*=\s*\d' -P
 exactly 0 "simulated = 0/1" 'simulated\s*=\s*\d' -P
 #exactly 2 "var/ in proc arguments" '(^/[^/].+/.+?\(.*?)var/' -P
 exactly 0 "tmp/ vars" 'var.*/tmp/' -P
-exactly 6 "uses of .len" '\.len\b' -P
+# exactly 6 "uses of .len" '\.len\b' -P  # yeah no this is some Bay bullshit
 exactly 16 "uses of examine()" '[.|\s]examine\(' -P # If this fails it's likely because you used '/atom/proc/examine(mob)' instead of '/proc/examinate(mob, atom)' - Exception: An examine()-proc may call other examine()-procs
 exactly 7 "direct modifications of overlays list" '\boverlays((\s*[|^=+&-])|(\.(Cut)|(Add)|(Copy)|(Remove)|(Remove)))' -P
 exactly 0 "new/list list instantiations" 'new\s*/list' -P
