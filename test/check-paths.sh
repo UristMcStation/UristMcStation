46be79ec7c4f81fd5f4466c810fe65356d72ddc2
jmalek <jan.malek@gmail.com>
2025-05-05 14:26:29 +0100

    chore: 97->98 logs -_-

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -38,7 +38,7 @@ exactly 156 "to_world uses" '\sto_world\('
 exactly 0 "globals with leading /" '^/var' -P
 exactly 0 "globals without global sugar" '^var/(?!global/)' -P
 exactly 0 "apparent paths with trailing /" '\w/[,\)\n]' -P
-exactly 97 "to_world_log uses" '\sto_world_log\('
+exactly 98 "to_world_log uses" '\sto_world_log\('
 exactly 2 "world<< uses" 'world<<|world[[:space:]]<<'
 exactly 2 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
 exactly 16 "<< uses" '(?<!<)<<(?!<)' -P
