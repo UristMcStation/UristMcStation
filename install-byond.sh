6fb02ecff8731a9da542871b8bbf43ccc83550b6
CPU_Blanc <g6yd96zk@cpublanc.mailer.me>
2025-08-30 02:34:27 +0100

    chore: set user-agent

--- install-byond.sh
+++ install-byond.sh
@@ -8,7 +8,7 @@ else
   mkdir -p ~/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}
   cd ~/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}
   echo "Installing DreamMaker to $PWD"
-  wget -O byond.zip "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip"
+  wget --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36" -O byond.zip "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip"
   unzip -o byond.zip
   cd byond
   make here
